aa15ad520f6526e53c35ac45750468587bfd7393
Nilton Pimentel <niltonpimentel02@gmail.com>
2022-07-15 22:32:12 -0300

    updated host

--- web/django_k8s/settings.py
+++ web/django_k8s/settings.py
@@ -26,10 +26,17 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get("DEBUG")) == "1"
 
+ENV_ALLOWED_HOST = os.environ.get("ENV_ALLOWED_HOST")
+ALLOWED_HOSTS = []
+if ENV_ALLOWED_HOST:
+    ALLOWED_HOSTS = [ENV_ALLOWED_HOST]
+
+"""
 ENV_ALLOWED_HOSTS = os.environ.get("ENV_ALLOWED_HOSTS")
 ALLOWED_HOSTS = []
 if ENV_ALLOWED_HOSTS:
     ALLOWED_HOSTS = [ENV_ALLOWED_HOSTS]
+"""
 
 
 # Application definition
